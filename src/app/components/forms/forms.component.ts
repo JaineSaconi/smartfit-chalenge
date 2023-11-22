d0c4d655cf64656322c391a71c85793e39157c3d
Jaine Saconi <jaine_saconi@hotmail.com>
2023-11-22 19:20:30 -0300

    feat: forms with responsivity

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
+  results = [];
+  formGroup!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: true
+    })
+  }
+
+   onSubmit(): void {
+    console.log(this.formGroup.value);
+   }
+
+   onClean(): void {
+    this.formGroup.reset();
+   }
 }
