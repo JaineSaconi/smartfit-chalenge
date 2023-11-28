5212df8533d146798a9831803b1d51808445b0be
Jaine Saconi <jaine_saconi@hotmail.com>
2023-11-28 19:11:33 -0300

    chore: get list of cards

--- src/app/components/cards-list/cards-list.component.ts
+++ src/app/components/cards-list/cards-list.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
-import { Input } from '@angular/core'
+
 @Component({
   selector: 'app-cards-list',
   templateUrl: './cards-list.component.html',
